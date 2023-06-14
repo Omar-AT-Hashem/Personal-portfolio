3228ae5cef7482f895fa3b4c452b7637b593a0e9
Omar-AT-Hashem <omar.at.hashem@gmail.com>
2023-06-14 11:45:06 +0300

    created about and started portfolio

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,6 +1,8 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import SocialLinks from "./components/SocialLinks";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
 function App() {
   
 
@@ -8,6 +10,8 @@ function App() {
     <>
     <Navbar />
     <Home />
+    <About />
+    <Portfolio />
 
     <SocialLinks />
     </>
