2d30f3905b48d2502d7feac914c9ef699ee7f30b
unknown <omar.at.hashem@gmail.com>
2023-06-19 14:01:45 +0300

    changed contact me button text

--- portfolio/src/components/Contact.tsx
+++ portfolio/src/components/Contact.tsx
@@ -42,7 +42,7 @@ const Contact = () => {
               className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300"
               type="submit"
             >
-              Let's talk !
+              Contact me !
             </button>
           </form>
         </div>
