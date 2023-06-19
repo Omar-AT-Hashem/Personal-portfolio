621550b950dfcea42eabd5560d984433abaf5d7a
unknown <omar.at.hashem@gmail.com>
2023-06-19 13:09:57 +0300

    fixed resume not downloading on footer click

--- portfolio/src/components/Footer.tsx
+++ portfolio/src/components/Footer.tsx
@@ -31,7 +31,7 @@ const Footer = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "./public/resume.pdf",
+      href: "./resume.pdf",
       download: true,
     },
   ];
